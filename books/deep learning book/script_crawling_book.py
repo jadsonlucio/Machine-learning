cea3e243ea85b9159b35f2a7ff87884a866709a5
jadsonlucio <jadsonaluno@hotmail.com>
2019-05-30 10:57:58 -0300

    fix some bugs

--- books/deep learning book/script_crawling_book.py
+++ books/deep learning book/script_crawling_book.py
@@ -4,23 +4,27 @@
 
 """
 
+
 import pdfkit
-import urllib3
+
+from time import sleep
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-from requests import get
+
 
 PATH = "C:\Program Files\wkhtmltopdf"
 PATH_WKTHTMLTOPDF = PATH + "\\bin\\wkhtmltopdf.exe"
 CONFIG = pdfkit.configuration(wkhtmltopdf=PATH_WKTHTMLTOPDF)
 HTML_FILES_PATH = "html\\"
+PDF_FILES_PATH = "pdfs\\"
 
 ignored_tags = [("div", {"class":"robots-nocontent sd-block sd-social sd-social-icon sd-sharing"}),
 				("div", {"class":"sharedaddy sd-block sd-like jetpack-likes-widget-wrapper jetpack-likes-widget-loaded"}),
 				("div", {"id":"jp-relatedposts"}),
 				("nav", {"class":"navigation post-navigation"}),
 				("footer", {"id":"colophon"}),
-				("aside", {"id":"secondary"})]
+				("aside", {"id":"secondary"}),
+				("div", {"class":"col-md-4 col-sm-12 col-xs-12 branding-container"})]
 
 
 
@@ -56,19 +60,19 @@ def crawling_chapters_urls(urls):
 def save_book(book_name,urls):
 	cont = 0
 	urls = crawling_chapters_urls(urls)
-	files_paths = []
 	for url in urls:
 		html_chapter = crawling_chapter(url)
 		print(html_chapter)
-		path = HTML_FILES_PATH + "file_" + str(cont)
-		files_paths.append(path)
-		with open(path, "w") as f:
-			f.write(html_chapter.encode("utf-8").decode("utf-8"))
+		path = HTML_FILES_PATH + "file_" + str(cont) + ".html"
+		with open(path, "w", encoding="utf-8") as f:
+			f.write(html_chapter)
 
+		pdfkit.from_file(path, PDF_FILES_PATH+"cap_{0}.pdf".format(cont), configuration=CONFIG)
+		sleep(10)
 		cont+=1
 
 
-	pdfkit.from_file(files_paths, book_name, configuration=CONFIG)
+
 
 
 
